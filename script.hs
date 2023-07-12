3682807b79374f015859a77b9920d1491da4d233
Yui Nishizawa <yui.nishizawa@gmail.com>
2023-07-12 09:50:50 +0900

    First barebone working

--- script.hs
+++ script.hs
@@ -1,50 +1,63 @@
 #!/usr/bin/env stack
--- stack script --resolver nightly-2022-11-05 --compiler ghc-9.2.5 --package tagsoup --package bytestring --package hxt
+-- stack script --resolver nightly-2022-11-05 --compiler ghc-9.2.5 --package tagsoup --package bytestring --package hxt --package text
 
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as B
 import Data.Foldable
-import Data.List (isPrefixOf, isSubsequenceOf)
-import Data.Tree.NTree.TypeDefs
--- import Text.XML.HXT.DOM.TypeDefs
--- import Text.XML.HXT.Parser.XmlParsec
--- import Text.XML.HXT.Parser.HtmlParsec
+import Data.List (isSubsequenceOf)
 import Debug.Trace
 import System.Environment
 import Text.HTML.TagSoup
+import Data.Maybe (catMaybes)
+import Data.Text qualified as T
+import Data.Text (Text)
+import Data.Text.IO qualified as T
+import Data.Text.Encoding qualified as T
+import GHC.Exts (IsString)
 
 main :: IO ()
 main = do
   (fileName : _) <- getArgs
-  content <- B.readFile fileName
+  content <- T.readFile fileName
+  let c = T.replace "<div><div><br /></div></div>" "<div></div>" content
   -- let parsed = xread content
   -- let parsed = xreadDoc content
   -- let parsed = parseHtmlContent content
   -- print parsed
   -- testPrintTrees parsed
-  let parsed = parseTags content
+  let parsed = parseTags c
       -- out = mapTag <$> parsed
       out = visitTags parsed
   -- testPrintTags parsed
   testPrintTags out
-  B.writeFile "result.enex" $ renderTags out
+  -- B.writeFile "result.enex" $ renderTags out
+  T.writeFile "result.enex" $ T.replace newlinePlaceholder "\n" $  renderTags out
 
-type MyTag = Tag ByteString
+type MyTag = Tag Text
 
-isInnerDoc :: ByteString -> Bool
-isInnerDoc = B.isPrefixOf "<!DOCTYPE" . B.strip
+newlinePlaceholder :: IsString s => s
+newlinePlaceholder = "______NEWLINE______"
+
+isInnerDoc :: Text -> Bool
+isInnerDoc = T.isPrefixOf "<!DOCTYPE" . T.strip
+
+isCodeBlock :: Attribute Text -> Bool
+isCodeBlock (name, value) = name == "style" && "codeblock" `isSubsequenceOf` T.unpack value
+
+hasCodeBlock :: [Attribute Text] -> Bool
+hasCodeBlock = any isCodeBlock
 
 mapTag :: MyTag -> MyTag
 mapTag t@(TagOpen s attr) =
-  trace ("trace open:" <> B.unpack s) $
+  trace ("trace open:" <> T.unpack s) $
     if any isCodeBlock attr
       then TagOpen s [("style", "background: blue; display: block;")]
       else t
 -- TagOpen "p" [("style", "background: blue; display: block;")] else t
 mapTag t@(TagText s) =
-  trace ("trace text:{" <> B.unpack (B.take 100 s) <> "}") $
+  trace ("trace text:{" <> T.unpack (T.take 100 s) <> "}") $
     if isInnerDoc s
       then
         let parsed = parseTags s
@@ -52,17 +65,15 @@ mapTag t@(TagText s) =
       else t
 mapTag t = t
 
-isCodeBlock :: Attribute ByteString -> Bool
-isCodeBlock (name, value) = name == "style" && "codeblock" `isSubsequenceOf` B.unpack value
-
 visitTags :: [MyTag] -> [MyTag]
 visitTags [] = []
 visitTags (t : rest)
-  | TagText s <- t, isInnerDoc s = let 
-                                  in TagText (renderTags $ visitTags $ parseTags s):rest
-  | otherwise = trace ("visit:" <> show t) $ (t : visitTags rest)
+  | TagOpen s attr <- t, hasCodeBlock attr = let (x, y) = trace ("map code") $ mapCode rest
+                                                  in x ++ visitTags y
+  | TagText s <- t, isInnerDoc s = TagText (renderTags $ visitTags $ parseTags s):rest
+  | otherwise = trace ("visit:" <> show t) $ t : visitTags rest
 
-matchTags :: ByteString -> Int -> ([MyTag], [MyTag]) -> ([MyTag], [MyTag])
+matchTags :: Text -> Int -> ([MyTag], [MyTag]) -> ([MyTag], [MyTag])
 matchTags tagName matchCount (matched, (t : rest))
   | TagClose s <- t,
     s == tagName =
@@ -73,14 +84,31 @@ matchTags tagName matchCount (matched, (t : rest))
   | otherwise = matchTags tagName matchCount (matched', rest)
   where
     matched' = matched ++ [t]
-matchTags tagName _ (matched, []) = error $ "Failed to find closing match: " <> B.unpack tagName <> "\n" <> show matched
+matchTags tagName _ (matched, []) = error $ "Failed to find closing match: " <> T.unpack tagName <> "\n" <> show matched
 
 mapCode :: [MyTag] -> ([MyTag], [MyTag])
-mapCode tags = ((open : inner) ++ [close], rest)
+mapCode tags = ((open : (catMaybes $ mapInner <$> inner)) ++ [close], rest)
   where
     (inner, rest) = matchTags "div" 0 ([], tags)
-    open = TagOpen "p" [("style", "background: blue; display: block;")]
-    close = TagClose "p"
+    newTag = "pre"
+    -- open = TagOpen newTag [("style", "background: blue; display: block; width: 100%; white-space: pre-line")]
+    -- open = TagOpen newTag []
+    open = TagOpen newTag [("style", "background: blue; margin: 0; padding: 0; font-size: 12px;")]
+    close = TagClose newTag
+
+    -- Need p
+    -- mapInner (TagOpen "div" _) = Just $ TagOpen "p" [("style", "background: silver; display: block; width: 100%; white-space: pre-line; margin: 0; padding: 0")]
+    -- mapInner (TagClose "div") = Just $ TagClose "p" 
+    -- mapInner (TagOpen "br/" _) = Nothing
+    mapInner (TagOpen "div" _) = Just $ TagText ""
+    mapInner (TagClose "div") = Just $ TagText newlinePlaceholder
+
+    -- br may be 
+    mapInner (TagOpen "br" _) = Just $ TagText ""
+    mapInner (TagClose "br" ) = Just $ TagText ""
+    mapInner (TagOpen _ _) = Just $ TagText ""
+    mapInner (TagClose _) = Just $ TagText ""
+    mapInner t = Just t
 
 -- \| s == tagName, matchCount == 0 = matched
 -- \| s == tagName = matchTags tagName (matchCount - 1) (matched ++ [t]) tags
@@ -116,13 +144,13 @@ testPrintTags = traverse_ testPrintTag
 -- testPrintNode (XAttr s) = putStrLn "Attr:"
 -- testPrintNode (XError s t) = putStrLn "Error:"
 
-testPrintTag :: Tag ByteString -> IO ()
-testPrintTag (TagOpen s _) = B.putStrLn $ "<" <> s <> ">"
-testPrintTag (TagClose s) = B.putStrLn $ "</" <> s <> ">"
-testPrintTag (TagComment s) = B.putStrLn $ "comment:" <> s
-testPrintTag (TagWarning s) = B.putStrLn $ "warning:" <> s
+testPrintTag :: MyTag -> IO ()
+testPrintTag (TagOpen s _) = T.putStrLn $ "<" <> s <> ">"
+testPrintTag (TagClose s) = T.putStrLn $ "</" <> s <> ">"
+testPrintTag (TagComment s) = T.putStrLn $ "comment:" <> s
+testPrintTag (TagWarning s) = T.putStrLn $ "warning:" <> s
 testPrintTag (TagPosition x y) = putStrLn $ show x <> "," <> show y
 testPrintTag (TagText s) =
   if isInnerDoc s
     then testPrintTags (parseTags s)
-    else B.putStrLn $ "text:" <> s
+    else T.putStrLn $ "text:" <> s
