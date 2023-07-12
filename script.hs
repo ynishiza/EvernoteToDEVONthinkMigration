ca40fcd23ed9aed8c57ee280bb913fbf6f384a97
Yui Nishizawa <yui.nishizawa@gmail.com>
2023-07-13 07:21:27 +0900

    Cleanup

--- script.hs
+++ script.hs
@@ -1,83 +1,101 @@
 #!/usr/bin/env stack
--- stack script --resolver nightly-2022-11-05 --compiler ghc-9.2.5 --package tagsoup --package bytestring --package hxt --package text
-
+{-
+  stack script
+    --resolver nightly-2022-11-05 --compiler ghc-9.2.5
+    --package tagsoup
+    --package bytestring --package text
+    --package filepath
+-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use mapMaybe" #-}
 
 import Data.Foldable
+import Data.Function ((&))
 import Data.List (isSubsequenceOf)
-import Debug.Trace
-import System.Environment
-import Text.HTML.TagSoup
-import Data.Maybe (catMaybes)
-import Data.Text qualified as T
 import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Text.IO qualified as T
+import Debug.Trace
 import GHC.Exts (IsString)
+import System.Environment
+import System.FilePath
+import Text.HTML.TagSoup
 
 main :: IO ()
 main = do
-  (fileName : _) <- getArgs
-  content <- T.readFile fileName
-  -- 
-  let c = T.replace "<div><div><br /></div></div>" "<div></div>" content
-  -- let parsed = xread content
-  -- let parsed = xreadDoc content
-  -- let parsed = parseHtmlContent content
-  -- print parsed
-  -- testPrintTrees parsed
-  let parsed = parseTags c
-      -- out = mapTag <$> parsed
-      out = visitTags parsed
-  -- testPrintTags parsed
-  testPrintTags out
-  -- B.writeFile "result.enex" $ renderTags out
-  T.writeFile "result.enex" $ T.replace newlinePlaceholder "\n" $  renderTags out
+  (inputPath : _) <- getArgs
+  content <- T.readFile inputPath
 
-type MyTag = Tag Text
+  --
+  -- let c = T.replace "<div><div><br /></div></div>" "<div></div>" content
+  let fileName = takeBaseName inputPath
+      outPath = takeDirectory inputPath </> fileName <> "_out" <.> "enex"
+      tags = parseTags content
+      out = visitTags tags
+  renderTags out
+    -- & T.replace newlinePlaceholder "\n"
+    & T.writeFile outPath
 
-color :: IsString s => s
-color = "rgb(232, 232, 232)"
+-- testPrintTags out
 
-newlinePlaceholder :: IsString s => s
-newlinePlaceholder = "______NEWLINE______"
+type EvernoteTag = Tag Text
 
-isInnerDoc :: Text -> Bool
-isInnerDoc t = T.isPrefixOf "<!DOCTYPE" t' || T.isPrefixOf "<?xml version"  t'
-  where t' = T.strip t
+type EvernoteAttribute = Attribute Text
 
-isCodeBlock :: Attribute Text -> Bool
-isCodeBlock (name, value) = name == "style" && "codeblock" `isSubsequenceOf` T.unpack value
+-- newlinePlaceholder :: IsString s => s
+-- newlinePlaceholder = "______NEWLINE______"
 
-hasCodeBlock :: [Attribute Text] -> Bool
-hasCodeBlock = any isCodeBlock
+-- Evernote note content is a CDATA of the form
+--
+--   <![CDATA[<!DOCTYPE en-note SYSTEM ..><en-note> ... </en-note>]]>
+isNoteContentData :: Text -> Bool
+isNoteContentData t = "<!DOCTYPE en-note" `T.isPrefixOf` t'
+  where
+    t' = T.strip t
 
--- mapTag :: MyTag -> MyTag
--- mapTag t@(TagOpen s attr) =
---   trace ("trace open:" <> T.unpack s) $
---     if any isCodeBlock attr
---       then TagOpen s [("style", "background: blue; display: block;")]
---       else t
--- -- TagOpen "p" [("style", "background: blue; display: block;")] else t
--- mapTag t@(TagText s) =
---   trace ("trace text:{" <> T.unpack (T.take 100 s) <> "}") $
---     if isInnerDoc s
---       then
---         let parsed = parseTags s
---          in trace "trace code:" $ TagText (renderTags (mapTag <$> parsed))
---       else t
--- mapTag t = t
+codeBlockTag :: IsString s => s
+codeBlockTag = "div"
 
-visitTags :: [MyTag] -> [MyTag]
+-- Evernote code block is of the form
+--
+--    <div style="... en-codeblock:true;">
+isCodeBlockAttribute :: EvernoteAttribute -> Bool
+isCodeBlockAttribute (name, value) = name == "style" && "en-codeblock" `isSubsequenceOf` T.unpack value
+
+hasCodeBlockAttribute :: [EvernoteAttribute] -> Bool
+hasCodeBlockAttribute = any isCodeBlockAttribute
+
+codeBlockColor :: IsString s => s
+codeBlockColor = "rgb(232, 232, 232)"
+
+codeBlockStyle :: (IsString s, Semigroup s) => s
+codeBlockStyle = "background: " <> codeBlockColor <> ";font-family: Monaco, Menlo, Consolas, &quot;Courier New&quot;, monospace; font-size: 12px; color: rgb(51, 51, 51); border-radius: 4px; border: 1px solid rgba(0, 0, 0, 0.15)"
+
+horizontalLine :: [EvernoteTag]
+horizontalLine = [
+  TagOpen "div" [("style", "text-align: center;")], 
+  TagText "--------------------------------------------------------------------------------------------------------", 
+  TagClose "div"]
+
+visitTags :: [EvernoteTag] -> [EvernoteTag]
 visitTags [] = []
-visitTags (t : rest)
-  | TagOpen _ attr <- t, hasCodeBlock attr = let (x, y) = trace "visitTags: codeBlock" $ mapCode rest
-                                                  in x ++ visitTags y
-  | TagText s <- t, isInnerDoc s = trace "visitTags: text" $ TagText (renderTags $ visitTags $ parseTags s):visitTags rest
-  | otherwise = trace ("visitTags:" <> show t) $ t : visitTags rest
+visitTags (tag : rest)
+  | TagOpen name attr <- tag,
+    codeBlockTag == name,
+    hasCodeBlockAttribute attr =
+      let (codeBlock, rest') = trace "visitTags: codeBlock" $ mapCodeBlockAsHighlight rest
+       in codeBlock ++ visitTags rest'
+  | TagOpen "hr" _ <- tag = visitTags rest
+  | TagClose "hr" <- tag = horizontalLine ++ visitTags rest
+  | TagText text <- tag,
+    isNoteContentData text =
+      trace "visitTags: text" $ TagText (renderTags $ visitTags $ parseTags text) : visitTags rest
+  | otherwise =
+      trace ("visitTags:" <> show tag) $ tag : visitTags rest
 
-matchTags :: Text -> Int -> ([MyTag], [MyTag]) -> ([MyTag], [MyTag])
+matchTags :: Text -> Int -> ([EvernoteTag], [EvernoteTag]) -> ([EvernoteTag], [EvernoteTag])
 matchTags tagName matchCount (matched, t : rest)
   | TagClose s <- t,
     s == tagName =
@@ -90,71 +108,58 @@ matchTags tagName matchCount (matched, t : rest)
     matched' = matched ++ [t]
 matchTags tagName _ (matched, []) = error $ "Failed to find closing match: " <> T.unpack tagName <> "\n" <> show matched
 
-mapCode :: [MyTag] -> ([MyTag], [MyTag])
-mapCode tags = ((open : catMaybes (mapInner <$> inner)) ++ [close], rest)
+mapCodeBlockAsHighlight :: [EvernoteTag] -> ([EvernoteTag], [EvernoteTag])
+mapCodeBlockAsHighlight tags = ((openTag : mapContent content) ++ [closeTag], rest)
   where
-    (inner, rest) = matchTags "div" 0 ([], tags)
+    (content, rest) = matchTags codeBlockTag 0 ([], tags)
+
+    -- Note: map codeblock to <pre>
+    --
+    -- When the note is converted to rich text, we want to ensure that the entire code block is highlighted.
+    -- Using a simple <div> block does not work because:
+    --
+    -- a) only text is highlighted, not whole line
+    -- b) empty lines in the code block are not highlighted
+    --
+    -- Using <pre> seems to work
+    -- a) entire line is highlighted in <pre>
+    -- b) empty line is highlighted in <pre>
+    --    However, empty lines must be a new line char "\n", not <br>
     newTag = "pre"
-    -- open = TagOpen newTag [("style", "background: blue; display: block; width: 100%; white-space: pre-line")]
-    -- open = TagOpen newTag []
-    open = TagOpen newTag [("style", "background: " <> color <> ";font-family: Monaco, Menlo, Consolas, &quot;Courier New&quot;, monospace; font-size: 12px; color: rgb(51, 51, 51); border-radius: 4px; border: 1px solid rgba(0, 0, 0, 0.15)")]
-    close = TagClose newTag
+    openTag = TagOpen newTag [("style", codeBlockStyle)]
+    closeTag = TagClose newTag
 
-    -- Need p
-    -- mapInner (TagOpen "div" _) = Just $ TagOpen "p" [("style", "background: silver; display: block; width: 100%; white-space: pre-line; margin: 0; padding: 0")]
-    -- mapInner (TagClose "div") = Just $ TagClose "p" 
-    -- mapInner (TagOpen "br/" _) = Nothing
-    mapInner (TagOpen "div" _) = Just $ TagText ""
-    mapInner (TagClose "div") = Just $ TagText newlinePlaceholder
+    mapContent [] = []
+    -- Note: each line in a code block is represented by a div
+    -- e.g.
+    --    <div>x = 1</div>
+    --    <div>y = 1</div>
+    --    <div><div>z =1</div>          May also be nested sometimes, in which case the outer div doesn't count as a new line
+    --         <div>a=10</div></div>
+    --
+    mapContent (TagOpen "div" _ : xs) = mapContent xs
+    -- Note: treat as a single line if nested divs
+    mapContent (TagClose "div" : TagClose "div" : TagClose "div" : xs) = TagText "\n" : mapContent xs
+    mapContent (TagClose "div" : TagClose "div" : xs) = TagText "\n" : mapContent xs
+    -- mapContent (TagClose "div" : xs) = TagText newlinePlaceholder : mapContent xs
+    mapContent (TagClose "div" : xs) = TagText "\n" : mapContent xs
+    -- br is a close tag <br />
+    mapContent (TagClose "br" : xs) = mapContent xs
+    -- Drop other styling tags in code blocks
+    mapContent (TagOpen _ _ : xs) = mapContent xs
+    mapContent (TagClose _ : xs) = mapContent xs
+    mapContent (t : xs) = t : mapContent xs
 
-    -- br may is a close tag <br /> 
-    mapInner (TagClose "br" ) = Just $ TagText ""
-    mapInner (TagOpen _ _) = Just $ TagText ""
-    mapInner (TagClose _) = Just $ TagText ""
-    mapInner t = Just t
-
-testPrintTags :: [MyTag] -> IO ()
+testPrintTags :: [EvernoteTag] -> IO ()
 testPrintTags = traverse_ testPrintTag
 
-testPrintTag :: MyTag -> IO ()
+testPrintTag :: EvernoteTag -> IO ()
 testPrintTag (TagOpen s _) = T.putStrLn $ "<" <> s <> ">"
 testPrintTag (TagClose s) = T.putStrLn $ "</" <> s <> ">"
 testPrintTag (TagComment s) = T.putStrLn $ "comment:" <> s
 testPrintTag (TagWarning s) = T.putStrLn $ "warning:" <> s
 testPrintTag (TagPosition x y) = putStrLn $ show x <> "," <> show y
 testPrintTag (TagText s) =
-  if isInnerDoc s
+  if isNoteContentData s
     then testPrintTags (parseTags s)
     else T.putStrLn $ "text:" <> s
-
--- testPrintTrees :: XmlTrees -> IO ()
--- testPrintTrees = traverse_ testPrintTree
-
--- testPrintTree :: XmlTree -> IO ()
--- testPrintTree (NTree a t) = do
---   testPrintNode a
---   testPrintTrees t
-
--- testPrintNode :: XNode -> IO ()
--- testPrintNode (XText s) = putStrLn $ "XText:" <> s
--- testPrintNode (XBlob s) = putStrLn "Blob:"
--- testPrintNode (XCharRef s) = putStrLn "Char:"
--- testPrintNode (XEntityRef s) = putStrLn "Entity:"
--- testPrintNode (XCmt s) = putStrLn "Cmt:"
--- testPrintNode (XCdata s) =
---   if "<!DOCTYPE" `isPrefixOf` s
---     then testPrintTrees (xread s)
---     else putStrLn "CData:"
--- testPrintNode (XPi s t) = putStrLn "Pi:"
--- testPrintNode (XTag s t) = putStrLn "Tag:"
--- testPrintNode (XDTD s t) = putStrLn "DTD:"
--- testPrintNode (XAttr s) = putStrLn "Attr:"
--- testPrintNode (XError s t) = putStrLn "Error:"
---
--- \| s == tagName, matchCount == 0 = matched
--- \| s == tagName = matchTags tagName (matchCount - 1) (matched ++ [t]) tags
--- \| otherwise = matchTags tagName matchCount (matched ++ [t]) tags
--- matchTags tagName matchCount matched (t@(TagOpen s _):tags)
--- \| s == tagName = matchTags tagName (matchCount + 1) (matched ++ [t]) tags
--- \| otherwise = matchTags tagName matchCount (matched ++ [t]) tags
-
